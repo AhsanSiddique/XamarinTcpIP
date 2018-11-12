0c58deb7436418490e66af27204f5f873c28e466
AhsanSiddique <ahsan.siddiq@outlook.com>
2018-11-12 17:43:04 +0500

    Updated

---
+++ XFClient/XFClient/XFClient/Views/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Sockets;
+using Xamarin.Forms;
+
+
+namespace XamarinForms.Client
+{
+    public partial class MainPage : ContentPage
+	{
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void Connect_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                TcpClient client = new TcpClient(); 
+                    await client.ConnectAsync(IPAddress.Text, Convert.ToInt32(Port.Text));
+                if (client.Connected)
+                {
+                    Connection.Instance.client = client;
+                    Application.Current.MainPage = new NavigationPage(new OperationsPage());
+                    
+                   await DisplayAlert("Connected", "Connected to server successfully!", "Ok");
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Connection unsuccessful!", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ""+ex.ToString(), "Ok");
+            }
+        }
+    }
+}
---
+++ XFClient/XFClient/XFClient/Views/OperationsPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinForms.Client
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class OperationsPage : ContentPage
+	{
+        public OperationsPage ()
+		{
+			InitializeComponent ();
+		}
+            //Sleep Button Command
+        private void Sleep_Clicked(object sender, EventArgs e)
+        {
+            var client = Connection.Instance.client;
+            NetworkStream stream = client.GetStream();
+            String s = "SLP2";
+            byte[] message = Encoding.ASCII.GetBytes(s);
+            stream.Write(message, 0, message.Length);
+        }
+        //Shutdown Button Command
+        private void Shutdown_Clicked(object sender, EventArgs e)
+        {
+            var client = Connection.Instance.client;
+            NetworkStream stream = client.GetStream();
+            String s = "SHTD3";
+            byte[] message = Encoding.ASCII.GetBytes(s);
+            stream.Write(message, 0, message.Length);
+        }
+        //Screenshot Button Command
+        private void Screenshot_Clicked(object sender, EventArgs e)
+        {
+            var client = Connection.Instance.client;
+            NetworkStream stream = client.GetStream();
+            String s = "TSC1";
+            byte[] message = Encoding.ASCII.GetBytes(s);
+            stream.Write(message, 0, message.Length);
+            var data = getData(client);
+            imageView.Source = ImageSource.FromStream(() => new MemoryStream(data));
+        }
+
+        //Data Collecting from Server
+        public byte[] getData(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] fileSizeBytes = new byte[4];
+            int bytes = stream.Read(fileSizeBytes, 0, fileSizeBytes.Length);
+            int dataLength = BitConverter.ToInt32(fileSizeBytes, 0);
+
+            int bytesLeft = dataLength;
+            byte[] data = new byte[dataLength];
+
+            int buffersize = 1024;
+            int bytesRead = 0;
+
+            while (bytesLeft > 0)
+            {
+                int curDataSize = Math.Min(buffersize, bytesLeft);
+                if (client.Available < curDataSize)
+                    curDataSize = client.Available;//This save me
+
+                bytes = stream.Read(data, bytesRead, curDataSize);
+                bytesRead += curDataSize;
+                bytesLeft -= curDataSize;
+            }
+            return data;
+        }
+    }
+}
